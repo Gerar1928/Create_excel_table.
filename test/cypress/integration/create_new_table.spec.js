0e61ec0d6b8deaf36821f416757db076719efe2b
Gerar <Gerar1928@gmail.com>
2021-04-12 01:48:35 -0400

    Added first testing and change styles

---
+++ test/cypress/integration/create_new_table.spec.js
@@ -0,0 +1,95 @@
+describe('Create new table and add information', () => {
+    it('Create new table', () => {
+        cy.visit('http://localhost:3000/');
+
+        cy.get('#newRow')
+            .click();
+
+        cy.get('input[type="text"]')
+            .type('Names');
+        
+        cy.get('select')
+            .select('Text');
+        
+        cy.get('input[type="submit"]')
+            .click();
+
+        cy.get('td[class=text]')
+            .dblclick()
+            .type('Gerar Almonte{enter}');
+        
+        cy.get('.select-all')
+            .click();
+        
+        cy.get('#delete')
+            .click();
+
+        cy.get('#newColumn')
+            .click();
+
+        cy.get('input[type="text"]')
+            .type('Names');
+        
+        cy.get('select')
+            .select('Text');
+        
+        cy.get('input[type="submit"]')
+            .click();
+
+        cy.get('td[class=text]')
+            .dblclick()
+            .type('Gerar Almonte{enter}');
+    });
+
+    it( 'Add information to the table', () => {
+        
+        cy.get('#newColumn')
+            .click();
+
+        cy.get('input[type="text"]')
+            .type('Phone Number');
+    
+        cy.get('select')
+            .select('Number');
+
+        cy.get('input[type="submit"]')
+            .click();
+
+        cy.get('td[class=number]')
+            .dblclick()
+            .type('8498809514{enter}');
+
+        cy.get('#newColumn')
+            .click();
+
+        cy.get('input[type="text"]')
+            .type('Email');
+        
+        cy.get('select')
+            .select('Email');
+
+        cy.get('input[type="submit"]')
+            .click();
+
+        cy.get('td[class=email]')
+            .dblclick()
+            .type('Gerar1928@gmail.com{enter}');
+        
+        cy.get('#newColumn')
+            .click();
+
+        cy.get('input[type="text"]')
+            .type('GitHub Profile');
+        
+        cy.get('select')
+            .select('Url');
+
+        cy.get('input[type="submit"]')
+            .click();
+
+        cy.get('td[class=url]')
+            .dblclick()
+            .type('https://github.com/Gerar1928{enter}');
+
+    });
+});
